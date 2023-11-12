19c9303a5e77906172601975ea8a462bde1ab52f
Himanshu Udupa G <115914825+Himanshu-Udupa@users.noreply.github.com>
2023-11-12 09:09:58 +0530

    Create LeapYear.java

---
+++ Flow of Program/LeapYear.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter the year");
+        int year=in.nextInt();
+        if(isLeapYear(year))
+            System.out.println(year+" is a leap year");
+        else System.out.println(year+" is not a leap year");
+    }
+
+    private static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+    }
+}
